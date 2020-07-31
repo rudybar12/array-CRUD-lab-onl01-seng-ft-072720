bb2f976f950ca2a4fdebf107430119e4a3bdd10b
Rodolfo Barrientos <rudybar12@yahoo.com>
2020-07-31 22:57:38 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,41 @@
 def create_an_empty_array
-  
+  name = Array.new []
 end
 
 def create_an_array
-  
+  name = Array.new ["Adrian", "Damian", "Fabian", "Sebastian"]
+
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  name = Array.new ["Adrian", "Damian", "Fabian", "Sebastian"] << "arrays!"
+
+
 end
 
 def add_element_to_start_of_array(array, element)
+  name = Array.new ["Adrian", "Damian", "Fabian", "Sebastian"]
+  name.unshift["wow"]
   
+
 end
 
 def remove_element_from_end_of_array(array)
-  
+
 end
 
 def remove_element_from_start_of_array(array)
-  
+
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+
 end
 
 def retrieve_first_element_from_array(array)
-  
+
 end
 
 def retrieve_last_element_from_array(array)
-  
+
 end
